6d4058356b2477a36844ba66564a35da46aab7dd
plundration <pavol.a.komlos@gmail.com>
2026-09-03 00:48:57 +0200

    Astro github pages deploy

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_URL = 'https://pavolkomlos.com';
+export const SITE_URL = 'https://plundration.github.com';
 export const SITE_TITLE = 'pavol.komlos';
 export const SITE_DESCRIPTION =
     'TLDR: programátor, študent medicíny a molekulárnej biológie, autor biologickej olympiády, člen eduB, fotograf;';
